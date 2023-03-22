7f7733a883830d990cc2cf394f6af7120ecf509f
yoshik-amber <yoshiki.kajiwara@link-u.co.jp>
2023-03-23 04:03:40 +0900

    画像差し込み

--- app/src/main/java/com/ingentity/movie/ui/home/HomePage.kt
+++ app/src/main/java/com/ingentity/movie/ui/home/HomePage.kt
@@ -3,6 +3,7 @@ package com.ingentity.movie.ui.home
 import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,29 +15,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.os.bundleOf
+import com.ingentity.movie.R
 
 @Composable
 fun HomePage() {
 
     val context = LocalContext.current
-    var backgroundColor by remember {
-        mutableStateOf(Color.Blue)
+    var backgroundImage by remember {
+        mutableStateOf(R.drawable.tp_1)
     }
     var text by remember {
         mutableStateOf("")
     }
-    val speechData = mapOf<String, Pair<Color, String>>(
-        "speak0" to (Color.Blue to "そんな不気味で薄暗い道を2つのライトがゆっくりと進んでいく。"),
-        "speak1" to (Color.Red to "辺りをライトで照らして安堵するリュウ。リュウ「はあ、じゃあ帰ろう。もう今日は終わりだ」"),
-        "speak2" to (Color.Green to "天魔「ここが音楽室だね」"),
-        "speak3" to (Color.Black to "天魔は持っていたライトで音楽室の看板を照らした。"),
-        "speak4" to (Color.Gray to "リュウ「なあ、もう帰ろうぜ……？」怯えた声で天魔にすがるリュウ。")
+    val speechData = mapOf(
+        "speak0" to (R.drawable.tp_1 to "そんな不気味で薄暗い道を2つのライトがゆっくりと進んでいく。"),
+        "speak1" to (R.drawable.tp_1 to "辺りをライトで照らして安堵するリュウ。リュウ「はあ、じゃあ帰ろう。もう今日は終わりだ」"),
+        "speak2" to (R.drawable.tp_1 to "天魔「ここが音楽室だね」"),
+        "speak3" to (R.drawable.tp_2 to "天魔は持っていたライトで音楽室の看板を照らした。"),
+        "speak4" to (R.drawable.tp_2 to "リュウ「なあ、もう帰ろうぜ……？」怯えた声で天魔にすがるリュウ。")
     )
     LaunchedEffect(key1 = Unit, block = {
         var textToSpeech: TextToSpeech? = null
@@ -46,7 +50,7 @@ fun HomePage() {
                     override fun onStart(p0: String?) {
                         val data = speechData.get(p0)
                         if (data != null) {
-                            backgroundColor = data.first
+                            backgroundImage = data.first
                             text = data.second
                         }
                     }
@@ -73,8 +77,13 @@ fun HomePage() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(backgroundColor)
     ) {
+        Image(
+            painter = painterResource(id = backgroundImage),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
         Box(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
